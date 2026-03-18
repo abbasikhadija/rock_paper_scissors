96eb9b15d76683b7f7cfb50d964f08326a8df423
abbasikhadija <khadijaabbasi.bscsf24@iba-suk.edu.pk>
2026-03-19 02:56:04 +0500

    added getComputerChoice and getHumanChoice functions

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello, World!");
+
+function getComputerChoice(){
+    let n =Math.floor(Math.random()*10)%3;
+    if (n ===0){
+        return "rock";
+    }
+    else if (n ===1){
+        return "paper";
+    }
+    else if (n ===2){
+        return "scissors";
+    }
+}
+function getHumanChoice(){
+    let choice=parseInt(prompt("Enter your choice: 0 for rock, 1 for paper, 2 for scissors"));
+    if (choice ===0){
+        return "rock";
+    }
+    else if (choice ===1){
+        return "paper";
+    }
+    else if (choice ===2){
+        return "scissors";
+    }
+}
